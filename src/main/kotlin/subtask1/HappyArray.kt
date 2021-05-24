01daac1cf2016f0088029863a0f1fc39edf98d1a
Timur Syrlybayev <timur.syrlybayev@gmail.com>
2021-05-24 16:45:06 +0600

    init: solve task

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,27 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var flag = true
+        var arrC = sadArray
+
+        fun checkArray(arr: IntArray): IntArray {
+            flag = false
+            var arr1 = arr
+            for (i in 1..arr.size - 2) {
+                if(arr1[i - 1] + arr1[i + 1] < arr1[i]) {
+                    arr1[i] = -1
+                    flag = true
+                }
+            }
+
+            arr1 = arr1.filter({it > 0}).toIntArray()
+            arrC = arr1
+            if(flag == true) {
+                arrC = checkArray(arrC)
+            }
+            return arrC
+        }
+
+        return(checkArray(arrC))
     }
 }
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,25 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val map = mapOf("(" to ")", "<" to ">", "[" to "]")
+        var list: Array<String> = arrayOf<String>()
+        fun filterString(index: Int, key: Char) {
+            var counter = 0
+            for (j in  index + 1..inputString.length - 1) {
+                if (inputString[j] == key) {
+                    counter++
+                } else if (inputString[j].toString() == map["$key"] && counter > 0) {
+                    counter--
+                } else if (inputString[j].toString() == map["$key"] && counter == 0) {
+                    list += inputString.substring(index + 1, j)
+                }
+            }
+        }
+        for (i in 0..inputString.length - 1) {
+            if (inputString[i] == '(' || inputString[i] == '<' || inputString[i] == '['){
+                filterString(i, inputString[i])
+            }
+        }
+        return list
     }
 }
